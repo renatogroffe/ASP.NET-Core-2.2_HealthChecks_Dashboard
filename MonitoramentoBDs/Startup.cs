8ddf4b7a2ab38177103b194196391b4cf3cf4959
Renato Groffe <renato.groffe@yahoo.com.br>
2019-06-23 08:59:18 -0300

    ASP.NET Core 2.2 + Health Checks + Monitoramento de Bancos de Dados
    ASP.NET Core 2.2 + Health Checks + Monitoramento de Bancos de Dados

---
+++ MonitoramentoBDs/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using HealthChecks.UI.Client;
+
+namespace MonitoramentoBDs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Verificando a disponibilidade dos bancos de dados
+            // da aplicação através de Health Checks
+            services.AddHealthChecks()
+                .AddSqlServer(Configuration.GetConnectionString("BaseIndicadores"), name: "baseSqlIndicadores")
+                .AddSqlServer(Configuration.GetConnectionString("BaseProdutos"), name: "baseSqlProdutos")
+                .AddMongoDb(Configuration.GetConnectionString("ServerMongo"), name: "serverMongo")
+                .AddRedis(Configuration.GetConnectionString("CacheRedis"), name: "cacheRedis");                
+            services.AddHealthChecksUI();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Gera o endpoint que retornará os dados utilizados no dashboard
+            app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI();
+        }
+    }
+}
